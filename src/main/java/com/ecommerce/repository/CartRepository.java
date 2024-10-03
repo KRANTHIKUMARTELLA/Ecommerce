67afc8cdaf286307db0ffc2511f056270088c5ed
KRANTHI-TELLA <kranthitella1@gmail.com>
2024-10-02 21:21:22 -0500

    second commit

--- src/main/java/com/ecommerce/repository/CartRepository.java
+++ src/main/java/com/ecommerce/repository/CartRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface CartRepository extends JpaRepository<Cart,Integer> {
     //Cart findByUser_Id(Integer userId);
 
+    //hello
+
 }
